385ce1cb53923561d348c4bd80acd46e719c2c7f
Michelle Malfabon <89042600+michellemalf@users.noreply.github.com>
2022-10-25 18:59:10 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//Michelle Malfabon
+//Aug 15 2022
+
+//-----------MINI 6-----EVEN OR ODD------------//
+
+//Description: I declared the variable (result). The user will input a number and function will define if number is odd or even with if statement using the modulus. 
+
+int result;
+int playAgain;
+
+void PlayGame()
+{
+Console.WriteLine("Enter a number: ");
+result = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("The result is " + result);
+
+if (result % 2 == 0)
+{
+    Console.WriteLine(result + " is even!");
+}
+else
+{
+    Console.WriteLine(result + " is odd!");
+}
+Console.WriteLine("Enter 0 to play again.");
+playAgain= Convert.ToInt32(Console.ReadLine());
+if (playAgain == 0)
+{
+    PlayGame();
+}
+else{
+    Console.WriteLine("Goodbye.");
+}
+}
+PlayGame();
+
+
